fd4f750f6b606c5224783b93a92fd434025cc00d
Otto Seiskari <otto.seiskari@gmail.com>
2020-04-25 18:59:01 +0300

    Tweak remote cursor appearance

--- src/RemoteCursor.vue
+++ src/RemoteCursor.vue
@@ -25,6 +25,10 @@ function addCenter(p0, thing) {
   };
 }
 
+function dist(p0, p1) {
+  return Math.sqrt((p1.x - p0.x)**2 + (p1.y - p0.y)**2);
+}
+
 module.exports = {
   props: [ 'player', 'position' ],
   data: () => ({}),
@@ -40,18 +44,19 @@ module.exports = {
     dots() {
       const p0 = addCenter(this.player.stack.position, { width: 100, height: 50 });
       const p1 = this.position;
-      //const dist = Math.sqrt((p1.x - p0.x)**2 + (p1.y - p0.y)**2);
-      //const DOT_DIST_PX = 20;
-      //const n = Math.round(Math.max(1, dist / DOT_DIST_PX));
       const n = 20;
       const arr = [];
       const style = { ...this.cursorDynamicStyle };
       for (let i=0; i < n; ++i) {
         const s = { ...style };
         const pos = interpolate(p0, p1, i, n);
-        s.left = `${pos.x}px`;
-        s.top = `${pos.y}px`;
-        arr.push(s);
+        const MIN_START_DIST_PX = 20;
+        if (dist(p0, pos) > MIN_START_DIST_PX) {
+
+          s.left = `${pos.x}px`;
+          s.top = `${pos.y}px`;
+          arr.push(s);
+        }
       }
       return arr;
     }
